e69ff1b90ebdefd3bd3964cbf2b6d552603fb18f
Mohd Rejoan <wearecrx@gmail.com>
2022-01-23 11:49:48 +0600

    slider animation added

--- js/script.js
+++ js/script.js
@@ -1,144 +1,63 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+function Slider() {
+    // get all slider element
+    const getAllSlides = document.querySelectorAll("#slider .slides__item");
+    const [leftBtn, rightBtn] = document.querySelectorAll(
+        "#slider .slide__btn .btn"
+    );
+    const getActiveItem = document.querySelector(
+        "#slider .slides__item.active"
+    );
+    const slidesWrapper = document.querySelector(".slide__item__wrap");
+    // utility or helper function
+    function activeImgText() {
+        const getText = getActiveItem.querySelector(".slider__content");
+        const getImg = getActiveItem.querySelector(".img__container");
+        return [getText, getImg];
+    }
+    function removeClass(items, classList) {
+        items.forEach((item, index) => {
+            // item.classList.remove()
+        });
+    }
+    // slider functions data 
+    const slidesItemsList = [];
+    // control function
+    leftBtn.onclick = function () {
+        const [text, img] = activeImgText();
+        text.classList.add("textMountSlide");
+        img.classList.add("imgMountSlide");
+        setTimeout(() => {
+            text.classList.remove("textMountSlide");
+            img.classList.remove("imgMountSlide");
+        }, 1100);
+    };
 
-function plusSlides(n) {
-    showSlides((slideIndex += n));
+    rightBtn.onclick = function () {
+        const [text, img] = activeImgText();
+        text.classList.add("textUnmountSlide");
+        img.classList.add("imgUnmountSlide");
+        setTimeout(() => {
+            text.classList.remove("textUnmountSlide");
+            img.classList.remove("imgUnmountSlide");
+            const removedItem = getActiveItem;
+            getActiveItem.remove();
+            slidesWrapper.appendChild(removedItem);
+            snapShortSlides()[0].classList.add("active")
+        }, 1100);
+    };
+
+    function snapShortSlides() {
+        return document.querySelectorAll("#slider .slides__item");
+    }
+    // core slider function
+    const coreSlider = () => {
+        
+    }
+    return {
+        slideLeft: function () {
+            console.log("hello rejoan");
+        },
+    };
 }
 
-function currentSlide(n) {
-    showSlides((slideIndex = n));
-}
-
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-
-    var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {
-        slideIndex = 1;
-    }
-    if (n < 1) {
-        slideIndex = slides.length;
-    }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-}
-
-
-
-// const slides = document.querySelectorAll(".mySlides");
-// console.log(slides)
-
-
-// slides[0].addEventListener("swipe", () => {
-//   console.log('do something')
-// })
-
-// document.addEventListener("touched", () => {
-//   console.log('hello')
-// });
-
-
-class Swipe {
-    constructor(element) {
-        this.xDown = null;
-        this.yDown = null;
-        this.element =
-            typeof element === "string"
-                ? document.querySelector(element)
-                : element;
-
-        this.element.addEventListener(
-            "touchstart",
-            function (evt) {
-                this.xDown = evt.touches[0].clientX;
-                this.yDown = evt.touches[0].clientY;
-            }.bind(this),
-            false
-        );
-    }
-
-    onLeft(callback) {
-        this.onLeft = callback;
-
-        return this;
-    }
-
-    onRight(callback) {
-        this.onRight = callback;
-
-        return this;
-    }
-
-    onUp(callback) {
-        this.onUp = callback;
-
-        return this;
-    }
-
-    onDown(callback) {
-        this.onDown = callback;
-
-        return this;
-    }
-
-    handleTouchMove(evt) {
-        if (!this.xDown || !this.yDown) {
-            return;
-        }
-
-        var xUp = evt.touches[0].clientX;
-        var yUp = evt.touches[0].clientY;
-
-        this.xDiff = this.xDown - xUp;
-        this.yDiff = this.yDown - yUp;
-
-        if (Math.abs(this.xDiff) > Math.abs(this.yDiff)) {
-            // Most significant.
-            if (this.xDiff > 0) {
-                this.onLeft();
-            } else {
-                this.onRight();
-            }
-        } else {
-            if (this.yDiff > 0) {
-                this.onUp();
-            } else {
-                this.onDown();
-            }
-        }
-
-        // Reset values.
-        this.xDown = null;
-        this.yDown = null;
-    }
-
-    run() {
-        this.element.addEventListener(
-            "touchmove",
-            function (evt) {
-                this.handleTouchMove(evt).bind(this);
-            }.bind(this),
-            false
-        );
-    }
-}
-
-
-
-var swiper = new Swipe("#my-element");
-swiper.onLeft(function () {
-    console.log("You swiped left.");
-});
-swiper.run();
-var swiper = new Swipe("#my-element");
-swiper.onRight(function () {
-    console.log("You swiped right.");
-});
-swiper.run();
+const slider = new Slider();
